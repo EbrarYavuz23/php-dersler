2a09d5d8e675f6c8ca4af086b713b56b711ca480
EbrarYavuz23 <ebrar2312@gmail.com>
2025-09-06 15:18:35 +0300

    templates bitti

---
+++ courseapp/uygulama-9/partials/_navbar.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-lg bg-primary navbar-dark">
+    <div class="container">
+        <a href="/" class="navbar-brand">Courseapp</a>
+        <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+                <a href="" class="nav-link active">Anasayfa</a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link">Anasayfa</a>
+            </li>
+        </ul>
+    </div>
+</nav>

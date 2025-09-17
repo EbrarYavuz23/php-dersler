50033001918beccd7f17b15481ba293827d9bebe
EbrarYavuz23 <ebrar2312@gmail.com>
2025-09-17 21:59:36 +0300

    uygulama 11 düzeltildi

--- courseapp/uygulama-11/index.php
+++ courseapp/uygulama-11/index.php
@@ -1,27 +1,22 @@
-<?php 
-
+<?php
     require "libs/variables.php";
-
     require "libs/functions.php";
-
-    
 ?>
 
 <?php include "partials/_header.php" ?>
 <?php include "partials/_navbar.php" ?>
-    
-<div class="container my-3">
-        
-        <div class="row">
-            <div class="col-3">
-                <?php include "partials/_menu.php" ?>
-            </div>
-            <div class="col-9">
-                <?php include "partials/_title.php" ?>
-                <?php include "partials/_kurs-listesi.php" ?>      
-                </div>
-            </div>
-        </div>  
-</div>
 
+<div class="container my-3">
+
+    <div class="row">
+        <div class="col-3">
+            <?php include "partials/_menu.php" ?>
+        </div>
+        <div class="col-9">
+            <?php include "partials/_title.php" ?>
+            <?php include "partials/_courses.php" ?>
+        </div>
+    </div>
+
+</div>
 <?php include "partials/_footer.php" ?>
--- courseapp/uygulama-11/register.php
+++ courseapp/uygulama-11/register.php
@@ -1,143 +1,123 @@
-<?php 
-
+<?php
     require "libs/variables.php";
-
     require "libs/functions.php";
-
-    
 ?>
 
 <?php include "partials/_header.php" ?>
 <?php include "partials/_navbar.php" ?>
 
-
 <?php
-
-    $username =  $email = $password = $repassword = $city = $hobiler = " ";
-    $usernameErr = $emailErr = $passwordErr = $repasswordErr = $cityErr = $hobilerErr = " ";
+    $usernameErr = $emailErr = $passwordErr = $repasswordErr = $cityErr = $hobbiesErr = "";
+    $username = $email = $password = $repassword = $city = "";
+    $hobbies = [];
 
     if($_SERVER["REQUEST_METHOD"]=="POST") {
 
-        
-        if (empty($_POST["username"])) {
-            $usernameErr = "username gerekli alan";
+        if(empty($_POST["username"])) {
+            $usernameErr = "username gerekli alan.";
         } else {
-            $username = $_POST["username"];
+            $username = safe_html($_POST["username"]);
         }
 
-
-        
-        if (empty($_POST["email"])) { 
-            $emailErr = "email gerekli alan";
+        if(empty($_POST["email"])) {
+            $emailErr = "email gerekli alan.";
         } else {
-            $email = $_POST["email"];
+            $email = safe_html($_POST["email"]);
         }
 
-
-
-        if (empty($_POST["pasword"])) {
-            $passwordErr = "password gerekli alan";
+        if(empty($_POST["password"])) {
+            $passwordErr = "password gerekli alan.";
         } else {
-            $password = $_POST["password"];
-        }
-    
-
-         
-        if ($_POST["repasword"]!= $_POST["password"]) {
-            $repasswordErr = "parolalar eşleşmiyor";
-        } else {
-            $repassword = $_POST["repassword"];
+            $password = safe_html($_POST["password"]);
         }
 
-        
-        
-        
-        if ($_POST["city"]== -1) {
-            $cityErr = "şehir seçmelisin";
+        if($_POST["password"] != $_POST["repassword"]) {
+            $repasswordErr = "parola tekrar alanı eşleşmiyor.";
+        } else {
+            $repassword = safe_html($_POST["repassword"]);
+        }
+
+        if($_POST["city"] == -1 ) {
+            $cityErr = "sehir seçmelisiniz.";
         } else {
             $city = $_POST["city"];
         }
 
-        
-        
-        if (!isset($_POST["hobiler"])) {
-            $hobilerErr = "en az 1 hobi seçmelisin";
+        if(!isset($_POST["hobbies"])) {
+            $hobbiesErr = "hobi seçmelisiniz.";
         } else {
-            $hobiler = $_POST["hobiler"];
+            $hobbies = $_POST["hobbies"];
         }
+
+        print_r($hobbies);
+
+        echo $username;
+        
     }
 
 ?>
 
-
-    
 <div class="container my-3">
-        
-        <div class="row">
-            <div class="col-12">
-                <form action="register.php" method="post">
-                    <div class="mb-3">
-                        <label for="username">Kullanıcı Adı</label>
-                        <input type="text" name="username" class="form-control" value="<?php echo $username ?>">
-                        <div class="text-danger"><?php echo $usernameErr;?></div>
-                    </div>
 
-                    <div class="mb-3">
-                        <label for="email">E-Posta</label>
-                        <input type="email" name="email" class="form-control" value="<?php echo $email ?>">
-                        <div class="text-danger"><?php echo $emailErr;?></div>
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="password">Şifre</label>
-                        <input type="password" name="password" class="form-control">
-                        <div class="text-danger"><?php echo $passwordErr;?></div>
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="repassword">Şifre Tekrar</label>
-                        <input type="password" name="repassword" class="form-control">
-                        <div class="text-danger"><?php echo $repasswordErr;?></div>
-                    </div>
-                    
-                    <div class="mb-3">
-                        <label for="city">Şehir Seçiniz</label>
-                        <select name="city" class="form-select">
-                        <option value="-1">Şehir Seçiniz</option>  
-                        
-                        
-                        <?php foreach($sehirler as $plaka => $sehir): ?>    
-                            <option value="<?php echo $plaka;?>"
-                            <?php echo $city == $plaka ? "selected": "" ?> >
-                            <?php echo $sehir; ?></option>
-                        <?php endforeach; ?>  
-                        
-                    
-                        </select>
-                        <div class="text-danger"><?php echo $cityErr;?></div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="hobiler">Hobiler</label>
-                        <div class="form-check">
-                            <input type="checkbox" name="hobi[]" value="sinema" id="hobiler_0">
-                            <label for="hobiler_0" class="form-check-label">Sinema</label>
-                        </div>
-                        
-                        <div class="form-check">
-                            <input type="checkbox" name="hobi[]" value="spor" id="hobiler_1">
-                            <label for="hobiler_1" class="form-check-label">Spor</label>
-                        </div>
-                        
-                        <div class="form-check">
-                            <input type="checkbox" name="hobi[]" value="müzik" id="hobiler_2">
-                            <label for="hobiler_2" class="form-check-label">Müzik</label>
-                        </div>
-                        <div class="text-danger"><?php echo $hobilerErr;?></div>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Kayıt Ol</button>
-                </form>
+    <div class="row">
+        <div class="col-12">
+           <form action="register.php" method="post">
+            <div class="mb-3">
+                <label for="username">Kullanıcı Adı</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username;?>">
+                <div class="text-danger"><?php echo $usernameErr; ?></div>
             </div>
-        </div>  
-</div>
+            <div class="mb-3">
+                <label for="email">Eposta</label>
+                <input type="email" name="email" class="form-control" value="<?php echo $email;?>">
+                <div class="text-danger"><?php echo $emailErr; ?></div>
+            </div>
+            <div class="mb-3">
+                <label for="password">Parola</label>
+                <input type="password" name="password" class="form-control" value="<?php echo $password;?>">
+                <div class="text-danger"><?php echo $passwordErr; ?></div>
+            </div>
+            <div class="mb-3">
+                <label for="repassword">Parola Tekrar</label>
+                <input type="password" name="repassword" class="form-control">
+                <div class="text-danger"><?php echo $repasswordErr; ?></div>
+            </div>
+            <div class="mb-3">
+                <label for="city">Şehir</label>
+                <select name="city" class="form-select">
+                    <option value="-1" selected>Şehir Seçiniz</option>
 
+                    <?php foreach($sehirler as $plaka => $sehir): ?>
+                        <option 
+                            value="<?php echo $plaka; ?>" <?php echo $city == $plaka ? ' selected':'' ?>> <?php echo $sehir; ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+                <div class="text-danger"><?php echo $cityErr; ?></div>
+            </div>
+            <div class="mb-3">
+                <label for="hobiler">Hobiler</label>
+
+                <?php foreach($hobiler as $id => $hobi): ?>
+
+                    <div class="form-check">
+                        <input type="checkbox" name="hobbies[]" 
+                        value="<?php echo $id;?>" 
+                        id="hobbies_<?php echo $id;?>"
+                        <?php if (in_array($id, $hobbies)) echo 'checked' ?>
+                        >
+                        <label for="hobbies_<?php echo $id;?>" class="form-check-label"><?php echo $hobi;?></label>
+                    </div>
+
+                <?php endforeach; ?>
+                     
+               
+                <div class="text-danger"><?php echo $hobbiesErr; ?></div>
+            </div>
+            <button type="submit" class="btn btn-primary">Kayıt Ol</button>
+           </form>
+        </div>
+    </div>
+
+</div>
 <?php include "partials/_footer.php" ?>
